e89256eca7e7970ccaaa5aab9cce18cb0ef73049
sourjo-ghosh <joyghosh211@outlook.com>
2026-05-11 02:22:16 +0600

    trying fix bug

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,6 +5,9 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 const client = new MongoClient(process.env.MONGO_URI);
 const db = client.db();
 
+
+
+
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
     client,
